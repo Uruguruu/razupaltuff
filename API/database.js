5c6bfcf49bff567dca7dd51ccef03c4b317679b2
Potestas06 <Patrick@krispel.com>
2023-01-03 16:07:17 +0100

    made idk changes

--- API/database.js
+++ API/database.js
@@ -87,48 +87,6 @@ module.exports = function (file) {
     return 200, "Product deleted";
   };
 
-  this.add_to_cart = function (warenkorbID, produktID, userID) {
-    const insert = this.db.prepare(
-      "insert into Warenkorb (warenkorbID, produktID, userID) values (@warenkorbID, @produktID, @userID)"
-    );
-    insert.run({ warenkorbID, produktID, userID });
-    return 200, "Product added to cart";
-  };
-
-  this.get_cart = function (userID) {
-    const get_cart = this.db.prepare(
-      "select * from Warenkorb where userID = @userID"
-    );
-    return get_cart.all({ userID });
-  };
-
-  this.delete_from_cart = function (warenkorbID) {
-    const delete_from_cart = this.db.prepare(
-      "delete from Warenkorb where warenkorbID = @warenkorbID"
-    );
-    delete_from_cart.run({ warenkorbID });
-    return 200, "Product deleted from cart";
-  };
-
-  this.get_orders = function () {
-    return this.db.prepare("select * from UserOrders").all();
-  };
-
-  this.create_order = function (orderID, userID, warenkorbID) {
-    const insert = this.db.prepare(
-      "insert into UserOrders (orderID, userID, warenkorbID) values (@orderID, @userID, @warenkorbID)"
-    );
-    insert.run({ orderID, userID, warenkorbID });
-    return 200, "Order created";
-  };
-
-  this.change_order_status = function (orderID, done) {
-    const update = this.db.prepare(
-      "update UserOrders set done = @done where orderID = @orderID"
-    );
-    update.run({ done, orderID });
-  };
-
   this.close = function () {
     this.db.close((error) => {
       if (error) {
