267a10d9faeb96804c65e7f63960821ef38abc8a
Potestas06 <Patrick@krispel.com>
2023-01-04 09:00:15 +0100

    jjjaaaaa

--- API/test.js
+++ API/test.js
@@ -1,6 +1,6 @@
 const database = require("./database.js");
 const db = new database("./database.db");
-/*
+
 db.create_user(1, "John", "john@newMail.com", "1970-01-01", "mypassword");
 console.log(db.getUsers());
 db.create_product(1, "godProduct", "../img/logo.png", 12.5, "God product");
@@ -10,7 +10,7 @@ console.log(db.get_cart(1));
 db.update_cart(1, "ordered");
 console.log(db.get_cart(1));
 db.add_rating(1, 5, "2020-01-01", 1, 1, "God product");
-*/
+
 console.log(db.delete_rating(1));
 console.log(db.delete_from_cart(1));
 console.log(db.delete_product(1));
