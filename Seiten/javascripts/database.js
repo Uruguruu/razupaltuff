3cf1adc5ba12dcbcb50ebf16c68aee581770c5d0
Ghrf6 <111276076+Ghrf6@users.noreply.github.com>
2023-01-05 08:50:50 +0100

    yaml geändert

--- Seiten/javascripts/database.js
+++ Seiten/javascripts/database.js
@@ -1,1 +1,34 @@
-fetch('http://localhost:3004/')
+const mysql = require('mysql');
+
+// Verbindung zur Datenbank herstellen
+const connection = mysql.createConnection({
+  host: 'localhost', // Hostname der Datenbank
+  user: 'db_user', // Benutzername für die Datenbank
+  password: 'db_password', // Passwort für die Datenbank
+  database: 'db_name' // Name der Datenbank
+});
+
+// Verbindung testen
+connection.connect();
+
+// Funktion zum Abrufen eines Produkts anhand seiner ID
+const getProductById = (id) => {
+  // Promise erstellen, das bei erfolgreichem Abschluss das Produkt zurückgibt und bei einem Fehler eine Fehlermeldung
+  return new Promise((resolve, reject) => {
+    // Abfrage an die Datenbank senden, um das Produkt abzurufen
+    connection.query('SELECT * FROM products WHERE id = ?', [id], (error, results, fields) => {
+      if (error) {
+        // Fehler zurückgeben
+        reject(error);
+      } else {
+        // Ergebnisse zurückgeben
+        resolve(results[0]);
+      }
+    });
+  });
+};
+
+// Funktionen exportieren, damit sie von anderen Modulen verwendet werden können
+module.exports = {
+  getProductById
+};
