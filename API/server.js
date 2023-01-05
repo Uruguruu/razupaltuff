2c417db3cfde915988672a1b6300c90ace656f0a
Potestas06 <Patrick@krispel.com>
2023-01-05 09:30:11 +0100

    idk

--- API/server.js
+++ API/server.js
@@ -6,17 +6,13 @@ const db = new database("./database.db");
 db.connect("./database.db");
 const fs = require("fs");
 var bodyParser = require("body-parser");
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const session = require("express-session");
 const mysql = require("mysql2");
 const { response } = require("express");
 const multer = require("multer");
-
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 app.use(bodyParser.json());
 var keys = {};
@@ -27,14 +23,12 @@ app.listen(port, () => {
   hell = genAPIKey();
   console.log(hell);
 });
-
 const genAPIKey = () => {
   //create a base-36 string that contains 30 chars in a-z,0-9
   return [...Array(300)]
     .map((e) => ((Math.random() * 36) | 0).toString(36))
     .join("");
 };
-
 async function check_key(key, email) {
   if (keys[email].includes(key)) {
     return true;
@@ -50,8 +44,6 @@ app.post("/login", urlencodedParser, (req, res) => {
   // to login into your account
   make();
   async function make() {
-    console.log("sdvsdssssssssssssssssssss");
-    console.log(req.body);
     let { email, password } = req.body;
     console.log(email === "admin" && password === "12345");
     if (email === "admin" && password === "12345") {
@@ -80,7 +72,6 @@ app.post("/login", urlencodedParser, (req, res) => {
     console.log(keys);
   }
 });
-
 app.post("/logout", urlencodedParser, (req, res) => {
   let { email, key } = req.body;
   array_list = keys[email];
@@ -98,39 +89,38 @@ app.post("/upload_image", (req, res) => {
 });
 
 app.post("/create_product", (req, res) => {
+  // to login into your account
+  make(req, res);
   async function make(req, res) {
-    let { produktID, name, price, producer } = req.body;
+    let { produktID, name, imageData, price, producer } = req.body;
 
-    // Check if an image file was uploaded
-    let imageBlob = null;
-    if (req.file) {
-      // Read the contents of the uploaded file
-      imageData = req.file.buffer;
-      // Convert the contents to a base64-encoded string
-      imageBlob = Buffer.from(imageData).toString("base64");
-    }
-
-    var id = await db.get_new_produktID();
-    // Generate a new ID for the user
-    produktID = id["produktID"] + 1;
+    // Lese den Inhalt der hochgeladenen Datei in eine Variable
+    imageData = req.file.buffer;
+    // Wandeln  den Inhalt in einen BLOB um
+    const imageBlob = Buffer.from(imageData).toString("base64");
+    // Jetzt kannst du den BLOB (imageBlob) in deiner .db-Datei speichern
+    console.log(imageBlob);
     name = req.name;
     price = req.price;
     producer = req.producer;
-
-    // Check if a product with the same name already exists
-    if (db.product_exist(name)) {
+    db.create_product(produktID, name, imageBlob, price, producer);
+    let lowestIdp = null;
+    // Iterate through all existing products
+    for (let product of products) {
+      if (product.id < lowestIdp || lowestIdp === null) {
+        lowestIdp = product.id;
+      }
+    }
+    // Generate a new ID for the product
+    let newId = lowestIdp + 1;
+    if (product_exist(name)) {
       response = "product exist";
-      return res.send(response);
     } else {
-      db.create_product(produktID, name, imageBlob, price, producer);
+      db.create_product(imageBlob);
       response = "product added";
-      return res.send(response);
     }
   }
-
-  make(req, res);
 });
-
 app.post("/update_product", (req, res) => {
   make(req, res);
   async function make(req, res) {
@@ -162,7 +152,6 @@ app.post("/update_product", (req, res) => {
     }
   }
 });
-
 app.post("/create_user", urlencodedParser, (req, res) => {
   // to login into your account
   make(req, res);
@@ -194,7 +183,6 @@ app.post("/create_user", urlencodedParser, (req, res) => {
     }
   }
 });
-
 app.post("/update_user", urlencodedParser, (req, res) => {
   // to login into your account
   make(req, res);
@@ -235,19 +223,15 @@ app.post("/update_user", urlencodedParser, (req, res) => {
     }
   }
 });
-
 app.get("/admin", (req, res) => {
   res.sendFile(__dirname + "\\admin_login.html");
 });
-
 app.get("/get_html", (req, res) => {
   res.sendFile(__dirname + "\\createproduct.html");
 });
-
 app.get("/admin", (req, res) => {
   res.sendFile(__dirname + "\\admin_login.html");
 });
-
 app.get("/admin_page?:key", (req, res) => {
   if (!(aidmin_key === req.query.key)) {
     res.sendFile(__dirname + "\\admin_login.html");
@@ -255,29 +239,13 @@ app.get("/admin_page?:key", (req, res) => {
     res.sendFile(__dirname + "\\admin_page.html");
   }
 });
-
 app.post("/load", (req, res) => {
   // Get the file that was set to our field named "image"
   console.log(req.files);
   const { image } = req.files;
-
   // If no image submitted, exit
   if (!image) return res.sendStatus(400);
-
   // Move the uploaded image to our upload folder
   image.mv(__dirname + "/images/" + image.name);
-
   res.sendStatus(200);
-});
-
-app.get("/get_product", (req, res) => {
-  //get all products
-  res.send(db.getProducts());
-});
-
-app.get("/get_product_by_ID", (req, res) => {
-  //get the id in the request
-  var id = req.query.id;
-  //get the product with the id
-  res.send(db.getProductByID(id));
 });
