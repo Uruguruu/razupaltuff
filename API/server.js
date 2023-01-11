d159f54e37f11a0aacf9b373dcd04d58bbc6a2ec
Niel Mueller <niel.mueller@edu.tbz.ch>
2023-01-11 14:36:54 +0100

    idgf

--- API/server.js
+++ API/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3004;
+const port = 3005;
 const database = require("./database.js");
 const db = new database("./database.db");
 const fs = require("fs");
